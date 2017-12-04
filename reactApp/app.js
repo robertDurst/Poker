652d9b15a70717c510a3d7307e0b82785f9cc36a
natedana <natepdana@gmail.com>
2017-12-04 14:22:17 -0800

    sockets ready for development

--- reactApp/app.js
+++ reactApp/app.js
@@ -4,7 +4,7 @@ var ReactDOM = require('react-dom');
 const io = require('socket.io-client');
 var socket = io.connect('http://localhost:3000');
 //Root Component
-const AppContainer = require('../components/AppContainer');
+const AppContainer = require('./components/AppContainer');
 /* This can check if your electron app can communicate with your backend */
 // fetch('http://localhost:3000')
 // .then(resp => resp.text())
