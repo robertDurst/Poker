45254ef3f18efe57d1f30ca88255262b43fab4fb
robertdurst <rsdurst@colby.edu>
2017-12-05 14:41:49 -0800

    Landing page styled for V1

--- reactApp/components/Landing.js
+++ reactApp/components/Landing.js
@@ -1,18 +1,38 @@
-const React = require('react')
-const {Link} = require('react-router-dom');
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { RaisedButton } from 'material-ui';
+
 
 class Landing extends React.Component {
   constructor(props) {
     super(props)
+    this.state = { x: 0, y: 0 };
   }
+
+  _onMouseMove(e) {
+    this.setState({ x: e.screenX, y: e.screenY });
+  }
+
   render() {
+    const { x, y } = this.state;
     return (
-    <div className="LandingPage__container--overall">
+    <div className="LandingPage__container--overall" onMouseMove={this._onMouseMove.bind(this)}>
       <div className="LandingPage__container--header">
-        <h1>Landing</h1>
+
       </div>
       <div className="LandingPage__container--body">
-        <Link to='/lobby'> Lobby  </Link>
+          <div className="LandingPage__title_container">
+            <h1 className="LandingPage__title" style={{fontSize: y/5}}>Poker Game</h1>
+          </div>
+          <div className="LandingPage__connect_button_container">
+            <Link to='/lobby' >
+              <RaisedButton
+                label="Connect"
+              />
+            </Link>
+          </div>
+
+
       </div>
       <div className="LandingPage__container--footer">
       </div>
