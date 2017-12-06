df78ae40a9dfb39fb4c096f496502599fcd855fc
robertdurst <rsdurst@colby.edu>
2017-12-06 12:45:19 -0800

    Game lobby structure

--- reactApp/components/Lobby.js
+++ reactApp/components/Lobby.js
@@ -1,20 +1,44 @@
 const React = require('react')
 const {Link} = require('react-router-dom');
+import GameRoomTable from './GameRoomTable';
+import { RaisedButton } from 'material-ui';
+
 class Lobby extends React.Component {
   constructor(props) {
     super(props)
+    this.state = {
+      currentUser: {
+        username: '',
+        balance: ''
+      }
+    }
   }
   render() {
     return (
     <div className="LobbyPage__container--overall">
         <div className="LobbyPage__container--header">
-          <h1> Lobby</h1>
+          <div className="LobbyPage__hostbutton_top">
+            <RaisedButton
+              label="Host"
+            />
+          </div>
+          <h1 className="LobbyPage__username_top"> Welcome {this.state.currentUser.username}</h1>
+          <p className="LobbyPage__balance_top">Bank Account: {this.state.currentUser.balance} BTC</p>
         </div>
         <div className="LobbyPage__container--body">
-          <Link to='/game' > Game  </Link>
+          <div className="LobbyPage__gamehost_table_container">
+            <div className="LobbyPage__gamehost_table">
+              <GameRoomTable />
+            </div>
+
+          </div>
+          <div className="LobbyPage__body_footer_container">
+            <RaisedButton
+              label="Direct Game Connection"
+            />
+          </div>
         </div>
         <div className="LobbyPage__container--footer"></div>
-
     </div>
   )
   }
