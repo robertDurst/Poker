5e55cf18b4825db1d65a3561287fe560c034e4eb
natedana <natepdana@gmail.com>
2017-12-06 15:25:33 -0800

    stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: './reactApp/app.js',
@@ -17,9 +18,16 @@ module.exports = {
             presets: ['react', 'es2015']
           }
         }
-      }
+      },
+      {
+       test: /\.css/,
+       loaders: ExtractTextPlugin.extract('css?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]')
+     }
     ]
   },
+  plugins: [
+    new ExtractTextPlugin("styles2.css")
+  ]
   stats: {
     colors: true
   },
