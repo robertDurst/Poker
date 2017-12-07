9bdba50b0b6b3a38a27a7d8e5ceb39fd3b10b4d4
robertdurst <rsdurst@colby.edu>
2017-12-06 17:25:14 -0800

    Disconnect localtunnel and host extra server

--- backend/gameHost/server.js
+++ backend/gameHost/server.js
@@ -1,12 +1,21 @@
-var http = require('http');
+const http = require('http');
+let server;
 
 function startServer() {
-  http.createServer(function (req, res) {
+  server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Welcome to poker!');
   }).listen(9090, "127.0.0.1");
 }
 
+function closeServer() {
+  console.log(server);
+  server.close( (data) => {
+    console.log("CLOSED", data);
+  });
+}
+
 module.exports = {
-  startServer
+  startServer,
+  closeServer
 }
